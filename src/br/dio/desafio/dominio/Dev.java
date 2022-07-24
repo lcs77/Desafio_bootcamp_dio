7b43b83628e87dacabb4ee5da16d68734bd3c824
lusca <lcs.job77@gmail.com>
2022-07-23 21:34:56 -0300

    SEGUNDO COMMIT

---
+++ src/br/dio/desafio/dominio/Dev.java
@@ -0,0 +1,53 @@
+package br.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudos> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudos> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    private void progredir(){}
+    private void calcularTotalXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudos> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+
+    public void setConteudosInscritos(Set<Conteudos> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudos> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudos> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+}
