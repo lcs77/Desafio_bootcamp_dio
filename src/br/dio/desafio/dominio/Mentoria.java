bbafe51407dd0d32dcf9cec987f29e3561510c7c
lusca <lcs.job77@gmail.com>
2022-07-23 21:03:59 -0300

    PRIMEIRO COMMIT

---
+++ src/br/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,35 @@
+package br.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudos{
+
+    private LocalDate data;
+
+
+    @Override
+    public double calcularXp() {
+        return 0;
+    }
+
+
+    public LocalDate getData() {
+        return data;
+    }
+
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+
+}
